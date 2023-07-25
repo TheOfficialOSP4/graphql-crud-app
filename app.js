c2c364706b2b9e636988fe898bcf6ae83ee620e6
Joie Zhang <joie@princeton.edu>
2023-07-25 12:56:33 -0700

    debugging loginLink cookies attempt

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
